486c8bcca6d63e048ac13d6f775938fbbacf7e95
Ayush <ayushbahuguna006@gmail.com>
2026-03-28 21:38:43 +0530

    [FRONTEND]-Home page Setup

---
+++ Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Hero1 from './Hero1'
+import Footer from './Footer'
+
+function HomeComponent() {
+  return (
+    <div>
+        <Header/>
+        <Hero1/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default HomeComponent
