3846262a1a8af5c18670f5d1bdda7d800e42b594
Ayush <ayushbahuguna006@gmail.com>
2026-05-06 12:12:44 +0530

    [frontend] o-auth integration

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,37 +1,32 @@
+"use client"
+
 import React from 'react'
-import { FaLaptopCode } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa"
+import { signIn } from "next-auth/react"
 
 function Header() {
   return (
-    <>
-      <div className='flex gap-6 md:gap-10 p-1 pt-2 items-center justify-between md:pr-10 bg-[#0F162A] overflow-x-hidden'>
-        
-        <div className='flex items-center gap-8 md:gap-20 md:px-10 pl-2 min-w-0'>
-          
-          <div className='flex items-center gap-1 flex-shrink-0'>
-            <FaLaptopCode className='text-blue-300' size={20} />
-            <p className='text-xl text-blue-200 whitespace-nowrap'>CodeWiki</p>
-          </div>
+    <div className='flex gap-6 md:gap-10 p-1 pt-2 items-center justify-between md:pr-10 bg-[#0F162A] overflow-x-hidden'>
 
-          <div className='flex text-sm font-bold text-gray-500 gap-3 md:gap-5 mt-2 flex-wrap'>
-            <div>
-              <p className='cursor-pointer hover:text-green-500'>HOME</p>
-            </div>
-            <div>
-              <p className='cursor-pointer hover:text-green-500'>ABOUT</p>
-            </div>
-          </div>
-
+      <div className='flex items-center gap-8 md:gap-20 md:px-10 pl-2 min-w-0'>
+        <div className='flex items-center gap-1 flex-shrink-0'>
+          <FaLaptopCode className='text-blue-300' size={20} />
+          <p className='text-xl text-blue-200 whitespace-nowrap'>CodeWiki</p>
         </div>
 
-        <div className='flex text-sm items-center gap-3 md:gap-5 flex-shrink-0'>
-          <div className='border rounded-sm text-base px-2 border-white/10 text-blue-200 mt-2 p-1 cursor-pointer hover:bg-blue-500 whitespace-nowrap'>
-            <p>Login</p>
-          </div>
+        <div className='flex text-sm font-bold text-gray-500 gap-3 md:gap-5 mt-2 flex-wrap'>
+          <p className='cursor-pointer hover:text-green-500'>HOME</p>
+          <p className='cursor-pointer hover:text-green-500'>ABOUT</p>
         </div>
-
       </div>
-    </>
+
+      <button
+        onClick={() => signIn("github")}
+        className='border rounded-sm text-base px-2 border-white/10 text-blue-200 mt-2 p-1 cursor-pointer hover:bg-blue-500 whitespace-nowrap'
+      >
+        Login
+      </button>
+    </div>
   )
 }
 
