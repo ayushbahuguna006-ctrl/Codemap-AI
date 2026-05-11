6b9c8b96715449935aa73c99a1c01a34bb00bab6
Ayush <ayushbahuguna006@gmail.com>
2026-05-11 12:36:50 +0530

    [frontend] footer updated

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <div className='flex md:flex-row flex-col items-center gap-5 -translate-y-14 md:justify-around bg-[#031033]/60 min-h-screen'>
+            <div className='flex-col'>
+              <p className='text-blue-200'>CODEWIKI</p>
+              <p>ENGINE-4.0 STABLE</p>
+            </div>
+            <div className='flex md:flex-row flex-col gap-5'>
+              <p>DOCUMENTATION</p>
+              <p>PRIVACY</p>
+              <p>GITHUB</p>
+              <p>TWITTER</p>
+            </div>
+            <div>
+              <p>@2025 CodeMap Architecture Code Intelligence</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
