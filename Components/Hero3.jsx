7c4944b092eb8c086f4654cc29cb1d2d468f7e54
Ayush <ayushbahuguna006@gmail.com>
2026-05-11 11:09:54 +0530

    [frontend] herosection-3 layout completion

--- Components/Hero3.jsx
+++ Components/Hero3.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Hero3() {
   return (
-     <div className='bg-[#0B1326]/90 -translate-y-13 md:-translate-y-0 min-h-screen flex-col px-[4vw] '>
+     <div className='bg-[#0B1326]/90 -translate-y-13 md:-translate-y-0 h-fit flex-col px-[4vw] '>
       
-      <div className='flex items-center translate-y-6 md:translate-y-6 justify-between '>
+      <div className='flex items-center translate-y-6 md:translate-y-2 justify-between '>
         <p className='text-blue-200 font-semibold text-sm md:text-lg'>
           RECENT ARCHIVES
         </p>
@@ -13,18 +13,18 @@ function Hero3() {
           <p>GLOBAL REPOSITORY FEED</p>
         </div>
       </div>
-      <div className='flex-col flex md:flex-row md:justify-around gap-5'>
-          <div className='bg-[#3A4B6A]/70 flex-col h-[30vh] md:w-[50vh] w-[40vh] md:ml-0 ml-2 md:mt-20 mt-10'>
+      <div className='flex-col flex md:flex-row md:justify-around md:gap-5 py-[2vh]'>
+          <div className='bg-[#3A4B6A]/70 flex-col h-[30vh] md:w-[50vh] w-[40vh]  md:ml-0 ml-2 md:mt-10 mt-10'>
                <div className='flex'>
                       
                </div>
           </div>
-           <div className='bg-[#3A4B6A]/70 flex-col h-[30vh] w-[50vh] md:ml-0 ml-2 md:mt-20 mt-10'>
+           <div className='bg-[#3A4B6A]/70 flex-col h-[30vh] md:w-[50vh] w-[40vh] md:ml-0 ml-2 md:mt-10 mt-10'>
                <div className='flex'>
                       
                </div>
           </div>
-           <div className='bg-[#3A4B6A]/70 flex-col h-[30vh] w-[50vh] md:ml-0 ml-2 md:mt-20 mt-10'>
+           <div className='bg-[#3A4B6A]/70 flex-col h-[30vh] md:w-[50vh] w-[40vh] md:ml-0 ml-2 md:mt-10 mt-10'>
                <div className='flex'>
                       
                </div>
