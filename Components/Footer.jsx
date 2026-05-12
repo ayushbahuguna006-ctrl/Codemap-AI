d41f52a5e6896e7030a97353f99321066e681b33
Ayush <ayushbahuguna006@gmail.com>
2026-05-12 19:37:34 +0530

    [frontend] footer updated

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
 function Footer() {
+  //footer
   return (
     <div>
-        <div className='flex md:flex-row flex-col items-center gap-5 -translate-y-14 md:justify-around bg-[#031033]/60 min-h-screen'>
+        <div className='flex md:flex-row flex-col items-center gap-5  md:justify-around bg-[#031033]/60 h-[30vh]'>
             <div className='flex-col'>
-              <p className='text-blue-200'>CODEWIKI</p>
-              <p>ENGINE-4.0 STABLE</p>
+              <p className='text-blue-200 text-xl'>CodeWiki</p>
+              <p className='text-sm md:mt-2 text-gray-600'>ENGINE-4.0 STABLE</p>
             </div>
             <div className='flex md:flex-row flex-col gap-5'>
-              <p>DOCUMENTATION</p>
-              <p>PRIVACY</p>
-              <p>GITHUB</p>
-              <p>TWITTER</p>
+              <p className='text-sm text-gray-600'>DOCUMENTATION</p>
+              <p className='text-sm text-gray-600'>PRIVACY</p>
+              <p className='text-sm text-gray-600 '>GITHUB</p>
+              <p className='text-sm text-gray-600'>TWITTER</p>
             </div>
             <div>
-              <p>@2025 CodeMap Architecture Code Intelligence</p>
+              <p className='text-gray-600'>@2025 CodeMap Architecture Code Intelligence</p>
             </div>
         </div>
     </div>
